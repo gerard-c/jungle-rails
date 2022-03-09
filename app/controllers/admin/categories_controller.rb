ea2d89de9b79ce7fc26c3ece8d0c53f2f14a627f
Gerard Compion <gcompion93@gmail.com>
2022-03-09 17:59:00 -0500

    add routes for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,28 +1,2 @@
 class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV['AUTH_NAME'], password: ENV['AUTH_PASSWORD']
-
-  def index
-    @categories = Category.order(id: :desc).all
-  end
-
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_params)
-
-    if @product.save
-      redirect_to [:admin, :category], notice: 'Category created!'
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def category_params
-    params.require(:category).permit(:name)
-  end
-
 end
