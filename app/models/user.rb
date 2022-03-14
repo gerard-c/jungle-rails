393206e17cc061f69edd8836e437378e50d44377
Gerard Compion <gcompion93@gmail.com>
2022-03-14 16:59:35 -0400

    add minimum length password validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => true
   validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, length { minimum: 4 }
 
   def downcase_email
     self.email.downcase! if email.present?
