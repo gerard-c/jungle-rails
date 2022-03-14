425d6ef877fb338c1e1df863982f25468c620a7a
Gerard Compion <gcompion93@gmail.com>
2022-03-14 16:35:16 -0400

    setup user_spec file and add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  before_validation :downcase_email
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, :uniqueness => true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  def downcase_email
+    self.email.downcase!
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    before(:each) do
+      @user = User.new
+      @user.first_name = 'test'
+      @user.last_name = 'test'
+      @user.email = 'test@test'
+      @user.password = 'test'
+      @user.password_confirmation = 'test'
+    end
+  end
 end
