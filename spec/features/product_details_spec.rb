7a8a1d9b173a6b5243850b092811536c151bde5a
Gerard Compion <gcompion93@gmail.com>
2022-03-15 12:50:53 -0400

    setup test to confirm that visitors can click a link on a product and navigate to that product's page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "Visitor navigates to specific product page", type: :feature, js: true do
 
-    # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
   
@@ -17,14 +16,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
-    # ACT
-    visit root_path
-
-    # DEBUG
-    # save_screenshot
-
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+  scenario 'They see the product details' do
+    
   end
 end
